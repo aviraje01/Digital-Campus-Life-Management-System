5f6d2a7f825fad803ca013f841ceacd1be7c5ac0
Aviraje Bhosale <avirajebhosale0101@gmail.com>
2025-12-11 22:28:38 +0530

    Update assignment.js

--- assets/js/assignment.js
+++ assets/js/assignment.js
@@ -94,7 +94,8 @@ document.getElementById("log-out").addEventListener("click", function () {
         localStorage.clear();
 
         // Redirect to login page
-        window.location.href = "login.html";
+        window.location.href = "index.html";
     }
 });
+
 
